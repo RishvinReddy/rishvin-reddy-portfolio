f2cb3e2337ab963e79e59e866bc062786fd2416a
Rishvin Reddy <rishvinreddy@users.noreply.github.com>
2026-07-20 20:29:09 +0530

    chore: update sitemap domain to vercel url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://rishvinreddy.github.io';
+  const baseUrl = 'https://rishvin-reddy-portfolio.vercel.app';
   const lastModified = new Date();
 
   // All the primary routes in the application
